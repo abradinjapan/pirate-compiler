2382f7096d8f9acdcbbb3a5ab249d39499373d50
abradinjapan <abradinjapan@outlook.com>
2023-09-09 14:03:53 -0400

    Moved runner test into separate file.

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include "account.cpp"
 #include "generate.cpp"
 #include "run.cpp"
+#include "test-runner.cpp"
 
 std::string load_file(std::string file_path) {
     std::ifstream file(file_path);
@@ -85,65 +86,6 @@ runner::program compile(std::string user_code, bool& compilation_error) {
     return output;
 }
 
-void test_runner_gen_program(generator::workspace& workspace) {
-    // first abstraction
-    generator::write_instructions::write__create_new_context(workspace, 5);
-    generator::write_instructions::write__write_cell(workspace, (runner::cell)13, 0);
-    generator::write_instructions::write__write_cell(workspace, (runner::cell)1234, 1);
-    generator::write_instructions::write__write_cell(workspace, (runner::cell)9876, 2);
-    generator::write_instructions::write__pass_input(workspace, 1);
-    generator::write_instructions::write__pass_input(workspace, 2);
-    generator::write_instructions::write__jump_to_abstraction(workspace, 0);
-    generator::write_instructions::write__get_output(workspace, 4);
-    generator::write_instructions::write__get_output(workspace, 3);
-    generator::write_instructions::write__print_cell_as_number(workspace, 3);
-    generator::write_instructions::write__print_cell_as_number(workspace, 4);
-    generator::write_instructions::write__restore_old_context(workspace);
-    generator::write_instructions::write__quit(workspace);
-    
-    // second abstraction
-    generator::write_instructions::write__create_new_context(workspace, 5);
-    generator::write_instructions::write__get_input(workspace, 2);
-    generator::write_instructions::write__get_input(workspace, 1);
-    generator::write_instructions::write__write_cell(workspace, 97, 0);
-    generator::write_instructions::write__print_cell_as_character(workspace, 0);
-    generator::write_instructions::write__print_cell_as_character(workspace, 0);
-    generator::write_instructions::write__print_cell_as_character(workspace, 0);
-    generator::write_instructions::write__print_cell_as_character(workspace, 0);
-    generator::write_instructions::write__integer_add(workspace, 1, 0, 3);
-    generator::write_instructions::write__integer_add(workspace, 2, 0, 4);
-    generator::write_instructions::write__pass_output(workspace, 3);
-    generator::write_instructions::write__pass_output(workspace, 4);
-    generator::write_instructions::write__restore_old_context(workspace);
-    generator::write_instructions::write__jump_from_abstraction(workspace);
-}
-
-void test_runner() {
-    generator::workspace workspace;
-    bool error_occured = false;
-
-    // start first pass
-    workspace.start_pass_measure(1);
-
-    // measure program
-    test_runner_gen_program(workspace);
-
-    // finish first pass
-    workspace.finish_pass_measure();
-
-    // start second pass
-    workspace.start_pass_build();
-
-    // build program
-    test_runner_gen_program(workspace);
-
-    // finish second pass
-    workspace.finish_pass_build();
-
-    // run program
-    runner::run_program(workspace.p_program, error_occured);
-}
-
 void compile_and_run(std::string user_code) {
     runner::program program;
     bool compilation_error = false;
@@ -175,7 +117,7 @@ int main() {
     compile_and_run(load_file("programs/memory.pirate"));
 
     // test runner
-    //test_runner();
+    //test_runner::test_runner();
 
     return 0;
 }
